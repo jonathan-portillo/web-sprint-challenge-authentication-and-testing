6e43076603d694dcf25654e958b7ff63eabc4014
Jonathan Portillo <54336208+jonathan-portillo@users.noreply.github.com>
2020-12-14 01:15:45 -0500

    MVP

--- api/server.test.js
+++ api/server.test.js
@@ -1,1 +1,34 @@
-// Write your tests here
+const request = require("supertest");
+const server = require("./server");
+
+describe("server.js", () => {
+  test("make sure Server is working", () => {
+    expect(process.env.DB_ENV).toBe("testing");
+  });
+});
+
+describe("GET /", () => {
+  test("Returns 200", () => {
+    return request(server)
+      .get("/")
+      .then((res) => {
+        expect(res.status).toBe(200);
+      });
+  });
+
+  test("should return api is up", () => {
+    return request(server)
+      .get("/")
+      .then((res) => {
+        expect(res.body).toEqual({ api: "is up" });
+      });
+  });
+
+  test("JSON Body", () => {
+    return request(server)
+      .get("/")
+      .then((res) => {
+        expect(res.type).toBe("application/json");
+      });
+  });
+});
