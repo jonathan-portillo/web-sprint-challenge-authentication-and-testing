941a582e8b11aa25238a72dbb07955311388cc23
Jonathan Portillo <54336208+jonathan-portillo@users.noreply.github.com>
2020-12-13 22:40:20 -0500

    Login works!!

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -22,36 +22,26 @@ router.post("/register", async (req, res) => {
     console.log(err);
     res.status(500).json({ message: "Username already exists" });
   }
-  // res.end("implement register, please!");
-
-  /*
-    IMPLEMENT
-    You are welcome to build additional middlewares to help with the endpoint's functionality.
-
-    1- In order to register a new account the client must provide `username` and `password`:
-      {
-        "username": "Captain Marvel", // must not exist already in the `users` table
-        "password": "foobar"          // needs to be hashed before it's saved
-      }
-
-    2- On SUCCESSFUL registration,
-      the response body should have `id`, `username` and `password`:
-      {
-        "id": 1,
-        "username": "Captain Marvel",
-        "password": "2a$08$jG.wIGR2S4hxuyWNcBf9MuoC4y0dNy7qC/LbmtuFBSdIhWks2LhpG"
-      }
-
-    3- On FAILED registration due to `username` or `password` missing from the request body,
-      the response body should include a string exactly as follows: "username and password required".
-
-    4- On FAILED registration due to the `username` being taken,
-      the response body should include a string exactly as follows: "username taken".
-  */
 });
 
-router.post("/login", (req, res) => {
-  res.end("implement login, please!");
+router.post("/login", async (req, res) => {
+  let { username, password } = req.body;
+  try {
+    const user = await users.findBy({ username }).first();
+
+    if (user && bcryptjs.compareSync(password, user.password)) {
+      const token = generateToken({ username, password });
+      res
+        .status(200)
+        .json({ Message: `Welcome, ${user.username}`, token: token });
+    } else {
+      res.stutus(400).json("Username and password required!!!");
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json("invalid credentials ");
+  }
+
   /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
